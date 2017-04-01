344d80907f97d44ef873fa365a652cf2eef00dc7
schemr <schemryj@gmail.com>
2017-04-01 22:46:00 +0900

    Logging Service 구현

--- services/src/app/account/account.component.ts
+++ services/src/app/account/account.component.ts
@@ -1,3 +1,4 @@
+import { LoggingService } from '../logging.service';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
@@ -10,9 +11,10 @@ export class AccountComponent {
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private loggingService: LoggingService){}
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
   }
 }
--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service'
 
 @Component({
   selector: 'app-new-account',
@@ -8,11 +9,13 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
+  constructor(private loggingService: LoggingService){}
+
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
