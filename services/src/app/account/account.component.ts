398bb5cd560a99f26b58e54786d410efc8d11e79
schemr <schemryj@gmail.com>
2017-04-01 22:57:40 +0900

    Accounts Service 구현

--- services/src/app/account/account.component.ts
+++ services/src/app/account/account.component.ts
@@ -1,5 +1,6 @@
+import { AccountsService } from '../accounts.service';
 import { LoggingService } from '../logging.service';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -9,12 +10,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LoggingService){}
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService){}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountsService.updateAccount(this.id, status);
     this.loggingService.logStatusChange(status);
   }
 }
--- services/src/app/app.module.ts
+++ services/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 import { LoggingService } from './logging.service';
+import { AccountsService } from './accounts.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import { LoggingService } from './logging.service';
     FormsModule,
     HttpModule
   ],
-  providers: [LoggingService],
+  providers: [LoggingService, AccountsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
