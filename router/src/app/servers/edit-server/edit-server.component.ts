b46eb71203e67e01a1c99de922e7488b3152a123
schemr <schemryj@gmail.com>
2017-04-02 19:14:22 +0900

    queryParams handling 테스트

--- router/src/app/servers/edit-server/edit-server.component.ts
+++ router/src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -12,12 +12,18 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams, this.route.snapshot.fragment)
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true: false;
+      }
+    )
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
--- router/src/app/servers/server/server.component.ts
+++ router/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Subscription } from 'rxjs/Rx';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -12,7 +12,9 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit, OnDestroy {
   server: {id: number, name: string, status: string};
   serverParamSubscription: Subscription
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService, 
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
@@ -25,5 +27,8 @@ export class ServerComponent implements OnInit, OnDestroy {
   }
   ngOnDestroy() {
     this.serverParamSubscription.unsubscribe();
+  }
+  onEdit(){
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 }
