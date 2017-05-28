58edb876db83bd1f7881951347e52eef620d61e0
schemr <schemryj@gmail.com>
2017-05-28 18:56:01 +0900

    다양한 애니매이션 효과 적용(wildState)

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -24,15 +24,46 @@ import {
       })),
       transition('normal <=> highlighted', animate(300)),
       // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
     ])
 		]
 })
 export class AppComponent {
 	state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
 	onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+  
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 
 	onAdd(item) {
